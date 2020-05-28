ffea2c659b82fba30d91cbb1239da9a977c11995
AJAY487-star <64407115+AJAY487-star@users.noreply.github.com>
2020-05-28 14:21:42 -0700

    Delete fail_mail.py

--- fail_mail.py
+++
@@ -1,6 +0,0 @@
-import smtplib as sm
-msg = "OS is terminated"
-server=sm.SMTP_SSL("smtp.gmail.com", 465)
-server.login("ajaysoul282@gmail.com"," password")
-server.sendmail("ajaysoul282@gmail.com","ajaysoul282@gmail.com", msg)
-server.quit()
